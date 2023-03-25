6b9b8d3608307825498fd8830816ffa9a4067f00
Amnnnn16 <ag6835902@gmail.com>
2023-03-25 12:51:36 +0530

    First commit

---
+++ src/main/java/assignment_1/Main.java
@@ -0,0 +1,12 @@
+package assignment_1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+claculatorr ob=new claculatorr();
+System.out.println(ob.addition(10, 20));
+System.out.print(ob.div(10,20));
+	}
+
+}
---
+++ src/main/java/assignment_1/claculatorr.java
@@ -0,0 +1,18 @@
+package assignment_1;
+
+public class claculatorr {
+		public int addition(int x,int y) {
+			return x+y;
+		}
+		public int sub(int a,int b) {
+			return a-b;
+		}
+		public int multi(int a,int b) {
+			return a*b;
+		}
+		public int div(int a,int b) {
+			return a/b;
+		}
+	}
+
+
